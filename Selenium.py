0b54853e923136509367623c49fb0c3be7b00e7c
smurov.anatoliy <wertigo9026@gmail.com>
2022-10-11 16:28:21 +0300

    Ready to parse

--- Selenium.py
+++ Selenium.py
@@ -64,8 +64,8 @@ options.set_preference(
     'general.useragent.override',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.62 Safari/537.36',
 )
-driver = webdriver.Firefox(executable_path=path_drv, options=options) #Для скачанного локально драйвера браузера
-# driver = webdriver.Firefox(executable_path=auto_drv, options=options)
+# driver = webdriver.Firefox(executable_path=path_drv, options=options) #Для скачанного локально драйвера браузера
+driver = webdriver.Firefox(executable_path=auto_drv, options=options)
 wait = ww(driver, 10)
 
 
